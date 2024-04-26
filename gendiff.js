face8499c1834bd5dc3ec54f4a0659d639daaa22
Politova <anna_loginovskikh@mail.ru>
2024-04-26 16:24:23 +0500

    stage 2

--- gendiff.js
+++ gendiff.js
@@ -3,7 +3,10 @@ import { program } from 'commander';
 
 program
   .name('gendiff')
-  .description(' Compares two configuration files and shows a difference.')
-  .option(' -V, --version ', ' output the version number')
-  .helpOption('-h, --help ', ' output usage information');
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-V, --version ', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help ', 'output usage information');
 program.parse();

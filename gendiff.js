b473a598b6f4a750df94454de5662629a4e90815
Politova <anna_loginovskikh@mail.ru>
2024-04-26 16:18:56 +0500

    stage 1

--- gendiff.js
+++ gendiff.js
@@ -4,7 +4,6 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description(' Compares two configuration files and shows a difference.')
-  .version('1.0.0')
   .option(' -V, --version ', ' output the version number')
-  .option('-h, --help ', ' output usage information')
+  .helpOption('-h, --help ', ' output usage information');
 program.parse();

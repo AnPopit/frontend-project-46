4c304778bdd3c93b36f66fa0a95152a5c21a6d9b
Politova <anna_loginovskikh@mail.ru>
2024-05-29 19:34:19 +0500

    stage 6

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
-import parsing from './parsing.js';
+import parsing from './parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
   const filePath1 = path.resolve(process.cwd(), filepath1);
-  const file1 = fs.readFileSync(filePath1);
+  const file1 = fs.readFileSync(filePath1, 'utf8');
+  const format1 = path.extname(filePath1);
   const filePath2 = path.resolve(process.cwd(), filepath2);
-  const file2 = fs.readFileSync(filePath2);
-  const obj1 = parsing(file1);
-  const obj2 = parsing(file2);
+  const file2 = fs.readFileSync(filePath2, 'utf8');
+  const format2 = path.extname(filePath2);
+  const obj1 = parsing(file1, format1);
+  const obj2 = parsing(file2, format2);
   const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])];
   const result = keys.sort().map((key) => {
     if (!obj1.hasOwnProperty(key)) {
-      return `+ ${key}: ${obj2[key]} `;
+      return `  + ${key}: ${obj2[key]}`;
     }
     if (!obj2.hasOwnProperty(key)) {
-      return `- ${key}: ${obj1[key]} `;
+      return `  - ${key}: ${obj1[key]}`;
     }
     if (obj1[key] !== obj2[key]) {
-      return `- ${key}: ${obj1[key]}\n+ ${key}: ${obj2[key]}`;
+      return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
     }
-    if (obj1[key] == obj2[key]) {
-      return `  ${key}: ${obj1[key]} `;
+    if (obj1[key] === obj2[key]) {
+      return `    ${key}: ${obj1[key]}`;
     }
   });
   return `{\n${result.join('\n')}\n}`;
-  // результат сравнения двух файлов
-  // 1. Обработали пути path.resolve(process.cwd(), filepath1) это путь
-  // 2. Прочитали файлы readFileSync fs.readFileSync()
-  // 3. Получили расширения файлов есть  спец функция у path.
-  // 4. Парсинг данных передаю из 2 пункта и рачсширения из 3 пункта //это должны импортитровапть
 };
 
 export default genDiff;
-// compare в отдельном файле
--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,17 @@
 // const json = 'путь до файла'; // process.cwd() экпортировать в файлах, сюда импортировать и обернукть все в path.resolve([...paths])
 
 // const obj = JSON.parse(json);
+import yaml from 'js-yaml';
 
-const parsing = (file) => JSON.parse(file);
+const parsing = (file, format) => {
+  switch (format) {
+    case '.json':
+      return JSON.parse(file);
+    case '.yml':
+      return yaml.load(file, 'utf8');
+    case '.yaml':
+      return yaml.load(file, 'utf8');
+  }
+};
 
 export default parsing;
